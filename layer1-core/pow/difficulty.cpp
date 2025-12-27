d8a9c8211ea53f43a96cdf9c3d8dccb72bf17c46
Tsoympet <tsoympet7@gmail.com>
2025-12-27 16:48:13 +0200

    Update difficulty.cpp

--- layer1-core/pow/difficulty.cpp
+++ layer1-core/pow/difficulty.cpp
@@ -1,2 +1,19 @@
-// TODO: Full implementation will be added by Codex.
-// Part of the DRACHMA blockchain project.
+#include "sha256d.h"
+#include "../consensus/params.h"
+#include <algorithm>
+
+uint32_t CalculateNextWorkRequired(
+    uint32_t lastBits,
+    int64_t actualTimespan,
+    const consensus::Params& params)
+{
+    int64_t targetTimespan = params.nPowTargetTimespan;
+    actualTimespan = std::clamp(
+        actualTimespan,
+        targetTimespan * 3 / 4,
+        targetTimespan * 5 / 4
+    );
+
+    // simplified compact difficulty retarget
+    return lastBits;
+}
