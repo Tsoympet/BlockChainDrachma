5636555ffe5df5384ffef41df24782691a46e5be
Tsoympet <tsoympet7@gmail.com>
2025-12-27 16:46:41 +0200

    Update genesis.cpp

--- layer1-core/consensus/genesis.cpp
+++ layer1-core/consensus/genesis.cpp
@@ -1,2 +1,19 @@
-// TODO: Full implementation will be added by Codex.
-// Part of the DRACHMA blockchain project.
+#include "../block/block.h"
+#include "../crypto/tagged_hash.h"
+#include "params.h"
+
+Block CreateGenesisBlock(const consensus::Params& params) {
+    Transaction coinbase;
+    coinbase.vout.push_back({50 * 100000000ULL, params.genesisMessage});
+
+    Block genesis;
+    genesis.header.version = 1;
+    genesis.header.prevBlockHash = uint256::Zero();
+    genesis.header.time = params.nGenesisTime;
+    genesis.header.bits = params.nGenesisBits;
+    genesis.header.nonce = params.nGenesisNonce;
+    genesis.transactions.push_back(coinbase);
+    genesis.header.merkleRoot = ComputeMerkleRoot(genesis.transactions);
+
+    return genesis;
+}
