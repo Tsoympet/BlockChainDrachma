a8b855a9acf45022b64d0a812266d9d571bd0bd9
Tsoympet <tsoympet7@gmail.com>
2025-12-27 16:50:35 +0200

    Update merkle.cpp

--- layer1-core/merkle/merkle.cpp
+++ layer1-core/merkle/merkle.cpp
@@ -1,2 +1,27 @@
-// TODO: Full implementation will be added by Codex.
-// Part of the DRACHMA blockchain project.
+#include "merkle.h"
+
+uint256 ComputeMerkleRoot(const std::vector<Transaction>& txs) {
+    if (txs.empty()) return uint256::Zero();
+
+    std::vector<uint256> layer;
+    for (const auto& tx : txs)
+        layer.push_back(TaggedHash("TX", (const uint8_t*)&tx, sizeof(tx)));
+
+    while (layer.size() > 1) {
+        if (layer.size() % 2 != 0)
+            layer.push_back(layer.back());
+
+        std::vector<uint256> next;
+        for (size_t i = 0; i < layer.size(); i += 2) {
+            next.push_back(
+                TaggedHash("MERKLE",
+                    layer[i].data(),
+                    layer[i].size() + layer[i+1].size()
+                )
+            );
+        }
+        layer = next;
+    }
+    return layer[0];
+}
+
